6279ff7e6938d305e85d1790af4f5c434e4c4406
Aleš Daniel <ales.daniel@adpro.cz>
2020-06-15 14:05:58 +0200

    Added comments with urls to ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 # generate ssh keys TODO
+# https://dev.to/yashsway/setting-up-multiple-ssh-profiles-to-manage-multiple-git-accounts-macos-3m7m
+# https://medium.com/@xiaolishen/use-multiple-ssh-keys-for-different-github-accounts-on-the-same-computer-7d7103ca8693
