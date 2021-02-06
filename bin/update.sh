357ffc316fbd13adb9bd6fc6b09ea7cb6894a754
Aleš Daniel <ales.daniel@adpro.cz>
2021-02-06 09:49:04 +0100

    Update /bin/update.sh
    Add comments and make changes to some commands

--- bin/update.sh
+++ bin/update.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
 # brew
+echo "⬆️ Updating the formulae and Homebrew itself..."
 brew update
+echo "⏫ Upgrading installed formulae..."
 brew upgrade
 # brew cask upgrade deprecated
+echo "🔼 Upgrading installed cask formulae..."
 brew upgrade --cask
+echo "🔄 Cleaning old formulae..."
 brew cleanup
+echo "🔢 Checking system for potential problems..."
 brew doctor
 # brew cask doctor deprecated
-brew doctor --verbose
+# brew doctor --verbose
 
 # mac app store
+echo "⏫ Upgrading Mac App Store apps..."
 mas upgrade
 
 # system
+echo "⏫ Updating MacOS apps..."
+sudo softwareupdate -l
 sudo softwareupdate -i -a
 
 # show pinned apps versions
